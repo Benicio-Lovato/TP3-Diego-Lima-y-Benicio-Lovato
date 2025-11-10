3903476ab7a05ecd155f79b78fc836e831b6d69d
DiegoLima0 <diegoalanlima98@gmail.com>
2025-11-10 18:13:08 -0300

    hago index

--- MauEn3D - Grupo 6/views/index.php
+++ MauEn3D - Grupo 6/views/index.php
@@ -9,7 +9,16 @@
 <body>
     <div id="contenedor">
     <?php require_once "header.php"?>
-    <p>hola</p>
+    <p id="tuIdea">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nostrum fuga molestiae ab animi, laboriosam deserunt expedita distinctio beatae! Cupiditate ab</p>
+    <div id="centroIndex">
+        <div id="imagenesIndex">
+            <img src="../img/lewzrhcy.jpg" alt="modelo 3d" id="modelo1">
+            <img src="../img/pato3d.jpeg" alt="modelo 3d" id="modelo2">
+        </div>
+        <div id="centroTexto">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quasi fuga, dolore quos eveniet sunt nulla exercitationem, in maiores eaque vel distinctio esse veniam! Aspernatur consectetur delectus inventore ab sequi.</p>
+        </div>
+    </div>
     </div>
     <?php require_once "footer.php"?>
 </body>
